9ec540a404279fe43875e0e726874294fcde12d4
grogalskyj <47550651+grogalskyj@users.noreply.github.com>
2022-06-16 14:41:09 -0400

    Update build.gradle.kts

--- tense-lite/tense-lite/build.gradle.kts
+++ tense-lite/tense-lite/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
+	id("org.flywaydb.flyway") version "8.2.0"
 }
 
 group = "com.jahnelgroup.tense-lite"
