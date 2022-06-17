41c495a35f397d83b3841d31e8f019dd08a5cbd5
grogalskyj <47550651+grogalskyj@users.noreply.github.com>
2022-06-17 09:58:05 -0400

    Add files via upload

---
+++ tense-lite/src/main/kotlin/com/jahnelgroup/tenselite/tenselite/TenseLiteApplication.kt
@@ -0,0 +1,11 @@
+package com.jahnelgroup.tenselite.tenselite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TenseLiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<TenseLiteApplication>(*args)
+}
---
+++ tense-lite/src/test/kotlin/com/jahnelgroup/tenselite/tenselite/TenseLiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jahnelgroup.tenselite.tenselite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TenseLiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
